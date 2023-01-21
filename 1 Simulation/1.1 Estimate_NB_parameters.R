d34a75070c01d064b7be066777acc58e05f9f3c0
Edward-Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-01-21 13:43:17 +0800

    Estimate NB parameters from PBMC3K

--- 1 Simulation/1.1 Estimate_NB_parameters.R
+++ 1 Simulation/1.1 Estimate_NB_parameters.R
@@ -1,12 +1,16 @@
+# Estimate parameters of NB distribution from PBMC3K dataset --------------
 library(BiocParallel)
 library(BiocGenerics)
 library(parallel)
 library(MASS)
 library(nloptr)
-load("counts_lognorm.RData")
-load("pbmc_label.RData")
+library(Seurat)
+
+pbmc <- readRDS("pbmc_tutorial.rds")
 counts <- pbmc@assays$RNA@counts
+cluster.label <- pbmc@active.ident
 rm(pbmc)
+
 counts <- as.matrix(counts)
 counts0 <- counts[,cluster.label==levels(cluster.label)[1]]
 counts1 <- counts[,cluster.label==levels(cluster.label)[2]]
@@ -16,13 +20,16 @@ counts4 <- counts[,cluster.label==levels(cluster.label)[5]]
 counts5 <- counts[,cluster.label==levels(cluster.label)[6]]
 counts6 <- counts[,cluster.label==levels(cluster.label)[7]]
 rm(counts)
+
 fit.nb <- function(x){
   require(MASS)
   require(nloptr)
-# this function returns the MLE of mean and r
-obj.f <- function(theta){-sum(dnbinom(x,theta[2],mu = theta[1],log = T))}
-deriv.pl <- function(theta){nl.grad(x0 = theta,fn = obj.f)}
-nloptr(x0 = c(max(mean(x),1e-6),5),eval_f = obj.f,lb=rep(0,2),ub = rep(300,2),opts = list("algorithm"="NLOPT_LN_NELDERMEAD","ftol_rel" = 1e-9,"maxeval" = 5000,"maxtime" = 200,"xtol_rel" = 1e-4))$solution
+  # this function returns the MLE of mean and r
+  obj.f <- function(theta){-sum(dnbinom(x,theta[2],mu = theta[1],log = T))}
+  deriv.pl <- function(theta){nl.grad(x0 = theta,fn = obj.f)}
+  nloptr(x0 = c(max(mean(x),1e-6),5),eval_f = obj.f,lb=rep(0,2),ub = rep(300,2),
+         opts = list("algorithm"="NLOPT_LN_NELDERMEAD","ftol_rel" = 1e-9,
+                     "maxeval" = 5000,"maxtime" = 200,"xtol_rel" = 1e-4))$solution
 }
 
 cl <- makeCluster(getOption("cl.cores", 12))
@@ -34,4 +41,4 @@ counts.nb4 <- parApply(cl,counts4, 1, fit.nb)
 counts.nb5 <- parApply(cl,counts5, 1, fit.nb)
 counts.nb6 <- parApply(cl,counts6, 1, fit.nb)
 stopCluster(cl)
-save(counts.nb0,counts.nb1,counts.nb2,counts.nb3,counts.nb4,counts.nb5,counts.nb6,file = "counts_mean_r_2.RData")
+save(counts.nb0,counts.nb1,counts.nb2,counts.nb3,counts.nb4,counts.nb5,counts.nb6,file = "counts_mean_r.RData")
