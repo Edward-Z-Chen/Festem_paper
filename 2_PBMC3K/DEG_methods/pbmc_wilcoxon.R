88a690808232975c21f53665b61884d13aa52cd1
Edward Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-02-14 16:58:09 +0800

    Create pbmc_wilcoxon.R

---
+++ 2_PBMC3K/DEG_methods/pbmc_wilcoxon.R
@@ -0,0 +1,33 @@
+library(SingleCellExperiment)
+library(scater)
+library(stringr)
+library(stats)
+library(BiocParallel)
+library(BiocGenerics)
+library(parallel)
+library(MASS)
+library(nloptr)
+library(edgeR)
+library(biclust)
+pbmc <- readRDS("pbmc_tutorial.rds")
+load("pbmc_label.RData")
+cluster.label[c("CGGGCATGACCCAA-1","CTTGATTGATCTTC-1")] <- "Platelet"
+pbmc <- pbmc[,cluster.label!="Platelet"]
+cluster.label <- factor(cluster.label[cluster.label!="Platelet"])
+counts <- pbmc@assays$RNA@data
+rm(pbmc)
+counts <- as.matrix(counts)
+
+levels(cluster.label) <- 1:8
+cluster.labels <- cluster.label
+wil.result <- vector("list",8)
+my.wilcox <- function(x,label){
+  # labels should be T or F
+  wilcox.test(x[label],x[!label])$p.value
+}
+cl <- makeCluster(getOption("cl.cores", 12))
+for (i in 1:8){
+  wil.result[[i]] <- parApply(cl,counts,1,my.wilcox,label = (cluster.labels==i))
+}
+stopCluster(cl)
+save(wil.result,file = "./results/pbmc_wilcoxon.RData")
