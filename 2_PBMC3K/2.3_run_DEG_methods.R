aec964d545512d966e157a921ba9297c106a8e56
Edward Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-02-14 14:46:36 +0800

    Update analysis of PBMC3K

---
+++ 2_PBMC3K/2.3_run_DEG_methods.R
@@ -0,0 +1,19 @@
+source("./DEG_methods/pbmc_deseq2.R")
+rm(list = ls())
+
+full_flag <- F
+source("./DEG_methods/pbmc_edger.R")
+rm(list = ls())
+
+full_flag <- T
+source("./DEG_methods/pbmc_edger.R")
+rm(list = ls())
+
+source("./DEG_methods/pbmc_mast.R")
+rm(list = ls())
+
+source("./DEG_methods/pbmc_wilcoxon.R")
+rm(list = ls())
+
+source("./DEG_methods/pbmc_singleCellHaystack.R")
+rm(list = ls())
