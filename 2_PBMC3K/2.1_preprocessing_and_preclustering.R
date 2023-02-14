0b6e3dcccf8455e3367a160709518e03b7b6dab8
Edward Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-02-14 11:43:23 +0800

    Update analysis of PBMC3K

---
+++ 2_PBMC3K/2.1_preprocessing_and_preclustering.R
@@ -0,0 +1,30 @@
+library(Seurat)
+
+# Preprocessing -----------------------------------------------------------
+# Based on Seurat's tutorial. https://satijalab.org/seurat/articles/pbmc3k_tutorial.html
+# Load the PBMC dataset
+# The downloaded file should be placed under the same folder.
+pbmc.data <- Read10X(data.dir = "./")
+# Initialize the Seurat object with the raw (non-normalized data).
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
+pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+
+
+# Pre-clustering ----------------------------------------------------------
+## Using top 8000 genes selected by HVGvst.
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 8000)
+pbmc <- ScaleData(pbmc)
+pbmc <- FindNeighbors(pbmc, dims = 1:10)
+pbmc <- FindClusters(pbmc, resolution = 0.5)
+pbmc <- RunUMAP(pbmc, dims = 1:10)
+DimPlot(pbmc, reduction = "umap",label = T)
+cluster.labels <- pbmc@active.ident
+save(cluster.labels,file = "./results/pbmc_preclustering.RData")
+
+new.cluster.ids <- c("Naive CD4 T", "CD14+ Mono", "Memory CD4 T", "B", "CD8 T", "FCGR3A+ Mono", 
+                     "NK", "DC", "Platelet")
+names(new.cluster.ids) <- levels(pbmc)
+pbmc <- RenameIdents(pbmc, new.cluster.ids)
+DimPlot(pbmc, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+saveRDS(pbmc, file = "./results/pbmc3k.rds")
