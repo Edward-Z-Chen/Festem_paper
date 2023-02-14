fef1fe092fdd2144c4729f749b6f081621a65f8d
Edward Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-02-14 14:30:17 +0800

    Create 2.2_run_Festem.R

---
+++ 2_PBMC3K/2.2_run_Festem.R
@@ -0,0 +1,76 @@
+library(SingleCellExperiment)
+library(scater)
+library(stringr)
+library(stats)
+library(BiocParallel)
+library(BiocGenerics)
+library(parallel)
+library(MASS)
+library(nloptr)
+library(edgeR)
+library(EMDE)
+set.seed(321)
+cl <- makeCluster(getOption("cl.cores", 12))
+lusterSetRNGStream(cl, iseed = 321)
+
+pbmc <- readRDS("./results/pbmc3k.rds")
+load("pbmc_labels_hvg8000.RData")
+# load("pbmc_hvg8000_10groups.RData")
+# resolution = 0.15, pca = 1:10
+# load("pbmc_hvg8000_5groups.RData")
+
+counts <- pbmc@assays$RNA@counts
+raw.lib <- pbmc@meta.data$nCount_RNA
+rm(pbmc)
+counts <- as.matrix(counts)
+
+levels(cluster.labels) <- c(1:8,8)
+
+# Preprocessing -----------------------------------------------------------
+
+## Outlier
+rm.outlier <- function(x,percent){
+  tmp <- (x>=quantile(x,percent))
+  Q13 <- quantile(x[tmp],c(0.25,0.75))
+  if (Q13[2]==Q13[1]) Q13[2] <- Q13[1]+1
+  upper <- max(Q13[2]*3-2*Q13[1],1)
+  lower <- max(Q13[1]*3-2*Q13[2],0)
+  outlier.index <- tmp & (x>upper | x<lower)
+  mean.noout <- mean(x[tmp & (!outlier.index)])
+  x[outlier.index] <- round(mean.noout)
+  cat(sum(outlier.index),"\n")
+  x
+}
+
+counts <- t(parApply(cl,counts,1,rm.outlier,percent = 0.95))
+library.size <- calcNormFactors(counts,lib.size = raw.lib)
+sub.sample <- function(x){
+  library.size <- x[1]
+  x <- x[-1]
+  sample.count <- function(a,library.size){
+    rpois(1,a/library.size)
+  }
+  apply(matrix(x), 1, sample.count,library.size = library.size)
+}
+counts <- parApply(cl,rbind(library.size,counts),2,sub.sample)
+counts <- t(parApply(cl,counts,1,rm.outlier,percent = 0.85))
+rownames(counts) <- rownames(pbmc)
+
+# Only consider genes with non-zero expression in at least 30 cells
+nonzeros.num <- function(x){sum(x!=0)}
+tmp <- apply(counts, 1, nonzeros.num)
+counts <- counts[tmp>=30,]
+
+
+# Run Festem --------------------------------------------------------------
+alpha.label <- numeric(nlevels(cluster.labels)-1)
+for (g in 1:length(alpha.label)) {
+  alpha.label[g] <- sum(cluster.labels==g)/ncol(counts)
+}
+
+time.tmp <- Sys.time()
+em.result <- parApply(cl,counts,1,em.stat,alpha.ini=rbind(alpha.label,rep(1/8,7)),k0=100,C=1e-3,labels = cluster.labels,group.num = 8,prior.weight=0.05,earlystop = 1e-5)
+em.result9 <- parApply(cl,counts,1,em.stat,alpha.ini=rbind(alpha.label,rep(1/8,7)),k0=100,C=1e-3,labels = cluster.labels,group.num = 8,prior.weight=0.9,earlystop = 1e-5)
+print(paste0("Time: ",difftime(Sys.time(),time.tmp,units = "secs")))
+stopCluster(cl)
+save(em.result,em.result9,file = "pbmc_Festem.RData")
