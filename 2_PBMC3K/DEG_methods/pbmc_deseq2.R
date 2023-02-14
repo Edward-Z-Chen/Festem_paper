0ca114c10459ecbecb1099d29423695d11ca5141
Edward Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-02-14 15:51:42 +0800

    Update pbmc_deseq2.R

--- 2_PBMC3K/DEG_methods/pbmc_deseq2.R
+++ 2_PBMC3K/DEG_methods/pbmc_deseq2.R
@@ -9,26 +9,23 @@ library(parallel)
 library(MASS)
 library(nloptr)
 library(DESeq2)
-
-load("counts_lognorm_scexperi.RData")
-load("pbmc_label.RData")
+pbmc <- readRDS("pbmc3k.rds")
+pbmc <- as.SingleCellExperiment(pbmc)
+load("./results/pbmc3k_label.RData")
 cluster.label[c("CGGGCATGACCCAA-1","CTTGATTGATCTTC-1")] <- "Platelet"
 pbmc <- pbmc[,cluster.label!="Platelet"]
 cluster.label <- factor(cluster.label[cluster.label!="Platelet"])
-# levels(cluster.label) <- c(1,2,1,3,1,2,1,2)
 
-#pbmc <- as.SingleCellExperiment(pbmc)
 colData(pbmc) <- cbind(colData(pbmc),cluster.label)
-names(pbmc@colData@listData)[6] <- "condition"
+names(pbmc@colData@listData)[ncol(colData(pbmc))] <- "condition"
 
 snowparam <- SnowParam(workers = 12, type = "SOCK")
 register(snowparam, default = TRUE)
 registered()
 
-colData(pbmc)[,6] <- factor(colData(pbmc)[,6])
-levels(colData(pbmc)[,6]) <- 1:length(levels(colData(pbmc)[,6]))
+colData(pbmc)[,ncol(colData(pbmc))] <- factor(colData(pbmc)[,ncol(colData(pbmc))])
+levels(colData(pbmc)[,ncol(colData(pbmc))]) <- 1:length(levels(colData(pbmc)[,ncol(colData(pbmc))]))
 DE.counts <- DESeqDataSetFromMatrix(pbmc@assays@data@listData$counts,colData(pbmc),~condition)
 DEseq.results <- DESeq(DE.counts,parallel = T,quiet = T)
 DEseq.results <- results(DEseq.results,alpha = 0.05)
-# DEseq.results <- cbind(DEseq.results$padj,DEseq.results$pvalue)
-save(DEseq.results,file = "./pbmc_DEG/pbmc_DEseq2.RData")
+save(DEseq.results,file = "./results/pbmc_DEseq2.RData")
