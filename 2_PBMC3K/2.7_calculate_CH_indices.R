9360594bb719a1033b89b616257db8ed82351eca
Edward Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-02-15 20:35:32 +0800

    Update analysis of PBMC3K

---
+++ 2_PBMC3K/2.7_calculate_CH_indices.R
@@ -0,0 +1,24 @@
+library(Seurat)
+library(cluster)
+library(fpc)
+load("./results/pbmc3k_hvggenes.RData")
+pbmc <- readRDS("./results/pbmc3k.rds")
+ref <- pbmc@active.ident
+ref[c("CGGGCATGACCCAA-1","CTTGATTGATCTTC-1")] <- "Platelet"
+pbmc <- pbmc[,ref!="Platelet"]
+
+gene.list <- list(EM[1:1000],hvgvst[1:1000],hvgdisp[1:1000],dub,
+                  devianceFS[1:1000],trendvar[1:1000])
+CH.index <- rep(NA,length(gene.list))
+
+for (j in 1:ncol(SI.index)){
+  if (length(gene.list[[j]])>=1000){
+    gene.tmp <- gene.list[[j]]
+    pbmc <- RunPCA(pbmc, verbose = FALSE,features = gene.tmp)
+    pbmc <- FindNeighbors(object = pbmc, dims = 1:15)
+    pbmc <- FindClusters(object = pbmc, resolution = 1)
+    pca <- pbmc@reductions$pca@cell.embeddings[,1:15]
+    CH.index[j] <- calinhara(x=pca,clustering = as.numeric(label.list[[i]]))
+  }
+}
+save(CH.index,file = "./results/pbmc3k_CH.RData")
