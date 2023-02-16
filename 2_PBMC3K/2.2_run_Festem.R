eee77a3911c56af833127440e38f5328f35e6214
Edward Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-02-16 11:11:22 +0800

    Update 2.2_run_Festem.R

--- 2_PBMC3K/2.2_run_Festem.R
+++ 2_PBMC3K/2.2_run_Festem.R
@@ -71,3 +71,7 @@ em.result9 <- parApply(cl,counts,1,em.stat,alpha.ini=rbind(alpha.label,rep(1/8,7
 print(paste0("Time: ",difftime(Sys.time(),time.tmp,units = "secs")))
 stopCluster(cl)
 save(em.result,em.result9,file = "./results/pbmc_Festem.RData")
+
+## \gamma = 0.01
+em.result <- parApply(cl,counts,1,em.stat,alpha.ini=rbind(alpha.label,rep(1/8,7)),k0=100,C=1e-3,labels = cluster.labels,group.num = 8,prior.weight=0.01,earlystop = 1e-5)
+save(em.result,em.result9,file = "./results/pbmc_Festem_gamma0.01.RData")
