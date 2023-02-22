42e5a8f11d7aa51fe27003436fcce6af843906f0
Edward Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-02-22 10:47:28 +0800

    Update 3.8_construct_silver_standard.R

--- 3_IFNB/3.8_construct_silver_standard.R
+++ 3_IFNB/3.8_construct_silver_standard.R
@@ -4,17 +4,18 @@ load("./results/ifnb_ctrl_clustering_tSNE.RData")
 
 ifnb <- ifnb[,rownames(plots.list[[2]]$data)]
 ifnb <- NormalizeData(ifnb)
-umap_hvg <- umap.list[[2]]@cell.embeddings
-umap_hvg <- umap_hvg[rownames(plots.list[[2]]$data),]
+tsne_hvg <- tsne.list[[2]]@cell.embeddings
+tsne_hvg <- tsne_hvg[rownames(plots.list[[2]]$data),]
 
 # Housekeeping genes ------------------------------------------------------
 load("Housekeeping_GenesHuman.RData")
 Housekeeping_Genes <- dplyr::filter(Housekeeping_Genes,Gene.name%in%rownames(ifnb))
-moran_h <- RunMoransI(ifnb@assays$RNA@data[genes,],umap_hvg)
+genes <- Housekeeping_Genes$Gene.name
+moran_h <- RunMoransI(ifnb@assays$RNA@data[genes,],tsne_hvg)
 
 # Non-housekeeping genes --------------------------------------------------
 genes <- intersect(rownames(ifnb),Housekeeping_Genes$Gene.name)
-moran_nonh <- RunMoransI(ifnb@assays$RNA@data[genes,],umap_hvg)
+moran_nonh <- RunMoransI(ifnb@assays$RNA@data[genes,],tsne_hvg)
 
 
 # Non-zero expression percentage ------------------------------------------
