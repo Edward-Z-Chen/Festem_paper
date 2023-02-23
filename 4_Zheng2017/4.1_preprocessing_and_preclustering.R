1b73c6ef6522cf06aacd2dff1e4738696c6cd9dd
Edward Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-02-23 21:47:11 +0800

    Update analysis of Zheng(2017)

--- 4_Zheng2017/4.1_preprocessing_and_preclustering.R
+++ 4_Zheng2017/4.1_preprocessing_and_preclustering.R
@@ -36,3 +36,40 @@ save(cluster.labels,file = "./results/b1_preclustering_12g.RData")
 # Count matrix for TN test (python) ---------------------------------------
 counts <- as.matrix(pbmc@assays$RNA@counts)
 save(counts,file = "./results/b1_counts_forpython.RData")
+
+b2_exp <- read.table("b2_exprs.txt",header = T,row.names = 1,sep = "\t")
+pbmc <- CreateSeuratObject(b2_exp,project = "PBMC_2")
+rm(b2_exp)
+metadata <- read.table("b2_celltype.txt",header = T,sep = "\t")
+pbmc <- AddMetaData(pbmc,metadata$CellType, col.name = "celltype")
+rm(metadata)
+saveRDS(pbmc,file = "./results/b2.rds")
+
+# Preclustering -----------------------------------------------------------
+pbmc <- readRDS("./results/b2.rds")
+pbmc <- NormalizeData(pbmc)
+pbmc <- FindVariableFeatures(pbmc,nfeatures = 8000)
+pbmc <- ScaleData(pbmc)
+pbmc <- RunPCA(pbmc,features = VariableFeatures(pbmc),verbose = F)
+pbmc <- FindNeighbors(pbmc,dims = 1:20)
+pbmc <- FindClusters(pbmc,resolution = 0.4)
+pbmc <- RunUMAP(pbmc,dims = 1:20)
+DimPlot(pbmc,label = T)
+DimPlot(pbmc,label = T,group.by = "celltype")
+cluster.labels <- pbmc@active.ident
+levels(cluster.labels) <- 1:nlevels(cluster.labels)
+save(cluster.labels,file = "./results/b2_preclustering.RData")
+
+pbmc <- FindClusters(pbmc,resolution = 0.5)
+cluster.labels <- pbmc@active.ident
+levels(cluster.labels) <- 1:nlevels(cluster.labels)
+save(cluster.labels,file = "./results/b2_preclustering_16g.RData")
+
+pbmc <- FindClusters(pbmc,resolution = 0.2)
+cluster.labels <- pbmc@active.ident
+levels(cluster.labels) <- 1:nlevels(cluster.labels)
+save(cluster.labels,file = "./results/b2_preclustering_13g.RData")
+
+# Count matrix for TN test (python) ---------------------------------------
+counts <- as.matrix(pbmc@assays$RNA@counts)
+save(counts,file = "./results/b2_counts_forpython.RData")
