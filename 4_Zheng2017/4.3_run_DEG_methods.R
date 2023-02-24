53a3c6579f3d2294860a4787a5bf4a2fd231c026
Edward Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-02-24 08:24:46 +0800

    Update 4.3_run_DEG_methods.R

--- 4_Zheng2017/4.3_run_DEG_methods.R
+++ 4_Zheng2017/4.3_run_DEG_methods.R
@@ -1,22 +1,22 @@
-source("./DEG_methods/b1_deseq2.R")
+source("./DEG_methods/zheng_deseq2.R")
 rm(list = ls())
 
 full_flag <- F
-source("./DEG_methods/b1_edger.R")
+source("./DEG_methods/zheng_edger.R")
 rm(list = ls())
 
 full_flag <- T
-source("./DEG_methods/b1_edger.R")
+source("./DEG_methods/zheng_edger.R")
 rm(list = ls())
 
-source("./DEG_methods/b1_mast.R")
+source("./DEG_methods/zheng_mast.R")
 rm(list = ls())
 
-source("./DEG_methods/b1_wilcoxon.R")
+source("./DEG_methods/zheng_wilcoxon.R")
 rm(list = ls())
 
-source("./DEG_methods/b1_singleCellHaystack.R")
+source("./DEG_methods/zheng_singleCellHaystack.R")
 rm(list = ls())
 
-source("./DEG_methods/b1_ROSeq.R")
+source("./DEG_methods/zheng_ROSeq.R")
 rm(list = ls())
