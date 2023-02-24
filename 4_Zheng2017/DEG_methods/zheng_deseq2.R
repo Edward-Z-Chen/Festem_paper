051f71b2fdefd26d5474d6fc0e7ca019d08b709e
Edward Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-02-24 08:30:05 +0800

    Update zheng_deseq2.R

--- 4_Zheng2017/DEG_methods/zheng_deseq2.R
+++ 4_Zheng2017/DEG_methods/zheng_deseq2.R
@@ -10,6 +10,9 @@ library(MASS)
 library(nloptr)
 library(DESeq2)
 
+
+# Batch 1 -----------------------------------------------------------------
+
 pbmc <- readRDS("./results/b1.rds")
 pbmc <- pbmc[,!pbmc@meta.data$celltype%in%c("Hematopoietic stem cell","Megakaryocyte","Plasmacytoid dendritic cell")]
 
@@ -27,3 +30,22 @@ DEseq.results <- DESeq(DE.counts,parallel = T,quiet = T)
 DEseq.results <- results(DEseq.results,alpha = 0.05)
 # DEseq.results <- cbind(DEseq.results$padj,DEseq.results$pvalue)
 save(DEseq.results,file = "./results/b1_DEseq2.RData")
+
+# Batch 2 -----------------------------------------------------------------
+pbmc <- readRDS("./results/b2.rds")
+pbmc <- pbmc[,!pbmc@meta.data$celltype%in%c("Hematopoietic stem cell","Megakaryocyte","Plasmacytoid dendritic cell")]
+
+pbmc <- as.SingleCellExperiment(pbmc)
+names(pbmc@colData@listData)[4] <- "condition"
+
+snowparam <- SnowParam(workers = 12, type = "SOCK")
+register(snowparam, default = TRUE)
+registered()
+
+colData(pbmc)[,4] <- factor(colData(pbmc)[,4])
+levels(colData(pbmc)[,4]) <- 1:length(levels(colData(pbmc)[,4]))
+DE.counts <- DESeqDataSetFromMatrix(pbmc@assays@data@listData$counts,colData(pbmc),~condition)
+DEseq.results <- DESeq(DE.counts,parallel = T,quiet = T)
+DEseq.results <- results(DEseq.results,alpha = 0.05)
+# DEseq.results <- cbind(DEseq.results$padj,DEseq.results$pvalue)
+save(DEseq.results,file = "./results/b2_DEseq2.RData")
