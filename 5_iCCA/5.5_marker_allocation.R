c4bb8df6457ef0bb09c91d5bd384a3203623cf10
Edward Chen <78003795+Edward-Z-Chen@users.noreply.github.com>
2023-02-28 09:02:44 +0800

    Create 5.5_marker_allocation.R

---
+++ 5_iCCA/5.5_marker_allocation.R
@@ -0,0 +1,18 @@
+library(Seurat)
+library(BiocParallel)
+library(BiocGenerics)
+library(parallel)
+source("../utils/type_allocation.R")
+load("./results/iCCA_clustering_UMAP.RData")
+gene.names <- gene.list[[1]]
+nonepi <- readRDS("NonEpi.rds")
+nonepi <- NormalizeData(nonepi)
+nonepi@active.ident <- factor(label.list[[1]])
+names(nonepi@active.ident) <- colnames(nonepi)
+
+cl <- makeCluster(getOption("cl.cores", 20))
+nonepi.gene.allocation <- parApply(cl,nonepi@assays$RNA@data[gene.names,], 1, 
+                              type_allocate, type = factor(nonepi@active.ident),
+                              sig.level = 0.05)
+save(nonepi.gene.allocation,file = "./results/marker_allocation.RData")
+stopCluster(cl)
